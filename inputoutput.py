37aa569352b34eb1366b1ad17843514c5aea3147
Dhivakar-S <dhivadhivakar131@gmail.com>
2026-01-28 21:50:41 +0530

    Add files via upload

---
+++ inputoutput.py
@@ -0,0 +1,7 @@
+user_name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print("Hello,", user_name)
+print("You age is", age)
+print("You height is", height)
