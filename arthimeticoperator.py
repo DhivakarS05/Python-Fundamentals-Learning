1a7ee95531fcbe182457a3a9e276710c81c27630
Dhivakar-S <dhivadhivakar131@gmail.com>
2026-01-28 21:35:27 +0530

    Add files via upload

---
+++ arthimeticoperator.py
@@ -0,0 +1,8 @@
+a = 10
+b = 3
+print(a + b)      # Add
+print(a - b)      # Subtract
+print(a * b)      # Multiply
+print(a / b)      # Divide
+print(a // b)     # Floor division
+print(a % b)      # Remainder
